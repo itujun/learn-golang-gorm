e53e4400f06b9913a49f963e88a14b3bff0e942b
Juna <itujun7@gmail.com>
2025-08-04 12:34:02 +0700

    Model Stuct

--- todo.go
+++ todo.go
@@ -1,20 +1,15 @@
 package learn_golang_gorm
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
 // model todos
 type Todo struct {
-	ID          int64			`gorm:"column:id;primary_key;autoIncrement"`
+	gorm.Model
 	UserId      string			`gorm:"column:user_id"`
 	Title       string			`gorm:"column:title"`
 	Description string			`gorm:"column:description"`
-	Created_at  time.Time		`gorm:"column:created_at;autoCreateTime"`
-	Updated_at  time.Time		`gorm:"column:updated_at;autoCreateTime;autoUpdateTime"`
-	Deleted_at	gorm.DeletedAt 	`gorm:"column:deleted_at"`
 }
 
 // func (t *Todo) TableName() string {
